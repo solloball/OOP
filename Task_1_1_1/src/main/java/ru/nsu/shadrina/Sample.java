1c1480a39a58fb0b85da21b4d4af87fd528376ef
solloball <romanov.K48@gmail.com>
2023-09-10 14:25:57 +0700

    made task at first

--- Task_1_1_1/src/main/java/ru/nsu/shadrina/Sample.java
+++ Task_1_1_1/src/main/java/ru/nsu/shadrina/Sample.java
@@ -4,11 +4,44 @@ package ru.nsu.shadrina;
  * Sample class to simulate 1.1 task functionality
  */
 public class Sample {
-    public static void printHelloWorld() {
-        System.out.println("Hello world!");
+
+    public static void swap (int []arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+    public static void heapsort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--)  // make heap
+            heapify(arr, n, i);
+
+        for (int i = n - 1; i >= 0; i--) { // one by obe extract element from heap
+             swap(arr, i, 0);
+
+            heapify(arr, i, 0);  // length become i because some elements we extract
+        }
     }
 
-    public static void main(String[] args) {
-        printHelloWorld();
+    public static void heapify(int []arr, int n, int idx) {   // make
+        int idx_largest = idx; // the larges index is root index
+        int idx_left = (idx * 2) + 1; // index of left son
+        int idx_right = (idx * 2) + 2;  // index of right son
+
+        if (idx_right < n && arr[idx_largest] < arr[idx_right])
+            idx_largest = idx_right;
+
+        if (idx_left < n && arr[idx_largest] < arr[idx_left])
+            idx_largest = idx_left;
+
+        if (idx_largest != idx) {
+            swap(arr, idx, idx_largest);   // swap arr[idx] and arr[idx_largest] if they don't similar
+
+            heapify(arr, n, idx_largest);  //  we call recursively heapify
+        }
+    }
+    public static void main (String []args) {
+
     }
 }
+
--- Task_1_1_1/src/test/java/ru/nsu/shadrina/SampleTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/shadrina/SampleTest.java
@@ -2,23 +2,71 @@ package ru.nsu.shadrina;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SampleTest {
+    @Test
+    void firstTest() {
+        int []arr;
+        int []ans;
+        arr = new int[] {5, 4, 3, 2, 1};
+        ans = new int[] {1, 2, 3, 4, 5};
+        Sample.heapsort(arr);
+        assertArrayEquals(arr, ans);
+    }
+    @Test
+    void testWithoutElements() {
+        int []arr;
+        int []ans;
+        arr = new int[] {};
+        ans = new int[] {};
+        Sample.heapsort(arr);
+        assertArrayEquals(arr, ans);
+    }
 
     @Test
-    void checkMain() {
+    void testWithOneElement() {
+        int []arr;
+        int []ans;
+        arr = new int[] {1};
+        ans = new int[] {1};
+        Sample.heapsort(arr);
+        assertArrayEquals(arr, ans);
+    }
+    @Test
+    void testWithMaxAvailableValue() {
+        int []arr;
+        int []ans;
+        arr = new int[] {Integer.MAX_VALUE, 5, 6, 4, Integer.MIN_VALUE, 6, -56, 0, 43, -5};
+        ans = new int[] {Integer.MIN_VALUE, -56, -5, 0, 4, 5, 6, 6, 43, Integer.MAX_VALUE};
+        Sample.heapsort(arr);
+        assertArrayEquals(arr, ans);
+    }
+    @Test
+    void testMain() {
         Sample.main(new String[] {});
         assertTrue(true);
     }
 
     @Test
-    void checkPrinting() {
-        Sample.printHelloWorld();
+    void testSwap() {
+        int []arr;
+        arr = new int[] {5, 4, 3, 2, 1};
+        Sample.swap(arr, 0, 3);
+        assertEquals(2, arr[0]);
+        assertEquals(5, arr[3]);
     }
-
     @Test
-    void doINeedToCheckDefaultConstructor() {
-        final var sampleInstance = new Sample();
+    void testheapify() {
+        int []arr;
+        arr = new int[] {4, 7, 3, 5, 1};
+        Sample.heapify(arr, 0, 0);
+        assertArrayEquals(arr, new int[] {4, 7, 3, 5, 1});
+
+        Sample.heapify(arr, 3, 0);
+        assertArrayEquals(arr, new int[] {7, 4, 3, 5, 1});
     }
 }
