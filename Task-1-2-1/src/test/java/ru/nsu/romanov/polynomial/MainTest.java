777eb29b7d2c3ca4e3f5ef3d96a1f16fa3e92e39
solloball <romanov.K48@gmail.com>
2023-09-13 23:29:41 +0700

    setting gradle project

---
+++ Task-1-2-1/src/test/java/ru/nsu/romanov/polynomial/MainTest.java
@@ -0,0 +1,12 @@
+package ru.nsu.romanov.polynomial;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SampleTest {
+
+    @Test
+    void checkMain() {
+        assertTrue(true);
+    }
+}
