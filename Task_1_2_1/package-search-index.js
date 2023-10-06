9dd33f38817272606fc75e3724fed9b2206b8238
solloball <solloball@users.noreply.github.com>
2023-10-06 04:24:52 +0000

    Deploying to gh-pages from @ solloball/OOP@c5deeb33ff5a7ac4f5b794f63c276a3eae0b50de 🚀

--- Task_1_2_1/package-search-index.js
+++ Task_1_2_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.nsu.romanov.polynomial"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.nsu.romanov.tree"}]
--- Task_1_2_1/type-search-index.js
+++ Task_1_2_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.romanov.polynomial","l":"Main"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.romanov.tree","l":"Tree"}]
