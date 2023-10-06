12cdd93564071edca422e1fd544794490c3e94d6
solloball <romanov.K48@gmail.com>
2023-10-06 11:24:00 +0700

    first commit

---
+++ Task_1_2_1/src/main/java/ru/nsu/romanov/tree/Tree.java
@@ -0,0 +1,10 @@
+package ru.nsu.romanov.tree;
+
+/**
+ * Sample class to simulate 2.1 task functionality
+ */
+public class Tree {
+
+    public static void main(String[] args) {
+    }
+}
---
+++ Task_1_2_1/src/test/java/ru/nsu/romanov/tree/TreeTest.java
@@ -0,0 +1,12 @@
+package ru.nsu.romanov.tree;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SampleTest {
+
+    @Test
+    void doINeedToCheckDefaultConstructor() {
+    }
+}
