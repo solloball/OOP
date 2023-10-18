f82e72434fba6eb5a2923fe864520c624ef58521
solloball <romanov.K48@gmail.com>
2023-10-18 10:17:20 +0700

    changed order in import

--- Task_1_2_1/src/test/java/ru/nsu/romanov/tree/TreeTest.java
+++ Task_1_2_1/src/test/java/ru/nsu/romanov/tree/TreeTest.java
@@ -14,9 +14,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Random;
-import java.util.stream.Collectors;
 import java.util.Set;
-
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 
 
