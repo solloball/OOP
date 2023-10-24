a079924c21c8ae7db4cedef2409c06363bd49130
solloball <romanov.K48@gmail.com>
2023-10-24 11:15:48 +0700

    remove build.sh

--- Task_1_2_1/build.sh
+++
@@ -1,7 +0,0 @@
-javadoc -d docs ./src/main/java/ru/nsu/romanov/tree/Tree.java
-mkdir -p build
-javac -classpath /var/lib/flatpak/app/com.jetbrains.IntelliJ-IDEA-Community/x86_64/stable/a452749bc1fbba62f8ac245888cd85a41c0c13c13e9ecae637fd6c7170bd4fbb/files/IDEA-C/plugins/java/lib/resources/jdkAnnotations.jar ./src/main/java/ru/nsu/romanov/tree/Tree.java -d build
-cd build
-java ru.nsu.romanov.tree.Tree
-echo "completed successful"
-
