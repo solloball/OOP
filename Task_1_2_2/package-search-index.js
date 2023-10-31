eee4c9fb70ffb129937f24ea943594b1a8871917
solloball <solloball@users.noreply.github.com>
2023-10-31 14:52:40 +0000

    Deploying to gh-pages from @ solloball/OOP@76dae8ec9cb5267b51cf363ccb7303d31879a00d 🚀

--- Task_1_2_2/package-search-index.js
+++ Task_1_2_2/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.romanov.graph"},{"l":"ru.nsu.romanov.graph.interfaceGraph"},{"l":"ru.nsu.romanov.graph.matrix"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.romanov.graph"},{"l":"ru.nsu.romanov.graph.interfacegraph"},{"l":"ru.nsu.romanov.graph.matrix"}];updateSearchResults();
