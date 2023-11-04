d34fb4e08580fbeb1beed3bd5dde06c6af50e989
solloball <romanov.K48@gmail.com>
2023-11-04 22:04:08 +0700

    config project

---
+++ Task_1_3_1/src/main/java/ru/nsu/romanov/finder/Finder.java
@@ -0,0 +1,7 @@
+package ru.nsu.romanov.finder;
+
+/**
+ * Sample class to simulate 1.1 task functionality
+ */
+public class Finder {
+}
---
+++ Task_1_3_1/src/test/java/ru/nsu/romanov/finder/FinderTest.java
@@ -0,0 +1,13 @@
+package ru.nsu.romanov.finder;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FinderTest {
+
+    @Test
+    void checkMain() {
+        assertTrue(true);
+    }
+}
