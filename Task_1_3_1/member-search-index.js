571b234ad3c5b667f4c82e3d9bf318090507b4eb
solloball <solloball@users.noreply.github.com>
2023-11-13 14:41:17 +0000

    Deploying to gh-pages from @ solloball/OOP@fbbb69c999314596859ee39cd4311d08098f71d6 🚀

--- Task_1_3_1/member-search-index.js
+++ Task_1_3_1/member-search-index.js
@@ -1,1 +1,1 @@
-memberSearchIndex = [{"p":"ru.nsu.romanov.finder","c":"Finder","l":"Finder()","url":"%3Cinit%3E()"}]
+memberSearchIndex = [{"p":"ru.nsu.romanov.finder","c":"Finder","l":"find(String, String)","url":"find(java.lang.String,java.lang.String)"},{"p":"ru.nsu.romanov.finder","c":"Finder","l":"Finder()","url":"%3Cinit%3E()"}]
