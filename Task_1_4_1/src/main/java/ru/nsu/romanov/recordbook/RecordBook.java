d7f9589cf347d7b6be9468e05ac4c89f09d15c7e
solloball <romanov.K48@gmail.com>
2023-11-17 22:01:21 +0700

    configured project workspace

---
+++ Task_1_4_1/src/main/java/ru/nsu/romanov/recordbook/RecordBook.java
@@ -0,0 +1,7 @@
+package ru.nsu.romanov.recordbook;
+
+/**
+ * 
+ */
+public class RecordBook {
+}
---
+++ Task_1_4_1/src/test/java/ru/nsu/romanov/recordbook/RecorBookTest.java
@@ -0,0 +1,11 @@
+package ru.nsu.romanov.recordbook;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RecordBookTest {
+    @Test
+    void doINeedToCheckDefaultConstructor() {
+    }
+}
