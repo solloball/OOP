a9a9f28493be7868a93f1a85bb494c2480a805fc
solloball <romanov.K48@gmail.com>
2023-11-28 18:04:54 +0700

    fixed code

--- Task_1_4_1/src/main/java/ru/nsu/romanov/recordbook/RecordBook.java
+++ Task_1_4_1/src/main/java/ru/nsu/romanov/recordbook/RecordBook.java
@@ -8,7 +8,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
-
 import ru.nsu.romanov.recordbook.student.Student;
 import ru.nsu.romanov.recordbook.student.StudentIdx;
 import ru.nsu.romanov.recordbook.subject.Subject;
