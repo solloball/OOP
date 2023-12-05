71adee91bc245077e8a60832e1962f08e58bebc6
solloball <solloball@users.noreply.github.com>
2023-12-05 15:01:27 +0000

    Deploying to gh-pages from @ solloball/OOP@dc47b8e0e37c8f2bdd85f632e6c5d60be9ab3bef 🚀

---
+++ Task_1_5_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.romanov.calculator","l":"Calculator"},{"p":"ru.nsu.romanov.calculator","l":"TypeOperation"}];updateSearchResults();
