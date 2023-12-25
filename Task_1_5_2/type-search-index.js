53393a749f86568ab9a0398d07dc2afd039daa0e
solloball <solloball@users.noreply.github.com>
2023-12-25 04:51:25 +0000

    Deploying to gh-pages from @ solloball/OOP@3695b53b32c4833ef152bd1b24eec2f316b1e96a 🚀

---
+++ Task_1_5_2/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.romanov.note","l":"Note"},{"p":"ru.nsu.romanov.note","l":"NoteBook"},{"p":"ru.nsu.romanov.note","l":"TypeOperation"}];updateSearchResults();
