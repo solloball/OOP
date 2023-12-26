4123f4c5ed4c89df313c26b750a5095c407efd7a
solloball <109233909+solloball@users.noreply.github.com>
2023-12-26 08:50:24 +0700

    removed all task (#10)

--- Task_1_1_2/build.sh
+++
@@ -1,6 +0,0 @@
-javadoc -d docs ./src/main/java/ru/nsu/romanov/polynomial/Polynomial.java
-mkdir -p build
-javac ./src/main/java/ru/nsu/romanov/polynomial/Polynomial.java -d build
-cd build
-java ru.nsu.romanov.polynomial.Polynomial
-echo "completed successful"
--- Task_1_1_2/src/main/java/ru/nsu/romanov/polynomial/OperationType.java
+++
@@ -1,8 +0,0 @@
-package ru.nsu.romanov.polynomial;
-
-/**
- * main type operation with polynomial.
- */
-public enum OperationType {
-    ADD, SUBTRACT, MULT
-}
