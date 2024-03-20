3d5560dbda86d3ebf6eeebb5dc78542d572f0843
solloball <romanov.K48@gmail.com>
2024-03-20 16:52:49 +0700

    inital commit

---
+++ Task_2_3_1/src/main/java/ru/nsu/romanov/snake/Snake.java
@@ -0,0 +1,4 @@
+package ru.nsu.romanov.snake;
+
+public class Snake {
+}
---
+++ Task_2_3_1/src/test/java/ru/nsu/romanov/snake/SnakeTest.java
@@ -0,0 +1,8 @@
+package ru.nsu.romanov.snake;
+
+import org.junit.jupiter.api.Test;
+
+public class SnakeTest {
+    @Test
+    void test() {}
+}
