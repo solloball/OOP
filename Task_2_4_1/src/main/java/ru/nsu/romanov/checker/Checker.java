f74c08bbfbc72daba3826d1480fcc7d1aef0638d
solloball <romanov.K48@gmail.com>
2024-04-17 20:11:35 +0700

    init a project

---
+++ Task_2_4_1/src/main/java/ru/nsu/romanov/checker/Checker.java
@@ -0,0 +1,7 @@
+package ru.nsu.romanov.checker;
+
+public class Checker {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ Task_2_4_1/src/test/java/ru/nsu/romanov/checker/CheckerTest.java
@@ -0,0 +1,10 @@
+package ru.nsu.romanov.checker;
+
+import org.junit.jupiter.api.Test;
+
+public class CheckerTest {
+    @Test
+    void test() {
+
+    }
+}
